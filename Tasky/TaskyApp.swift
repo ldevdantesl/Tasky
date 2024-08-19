88e3c2c6a529714d3ae4282995fcf75140ce0a72
ldevdantesl <buzurg.berdiev@icloud.com>
2024-08-19 00:15:13 -0400

    Initial Commit

---
+++ Tasky/TaskyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskyApp.swift
+//  Tasky
+//
+//  Created by Buzurg Rakhimzoda on 19.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct TaskyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
